c5d341061efbc6de84e69fa8ca2eb9ca82f03746
Dirk Fraanje <46317263+dirkfraanje@users.noreply.github.com>
2022-06-26 20:49:52 +0200

    Progress bar added

--- day3.js
+++ day3.js
@@ -1,40 +1,49 @@
+'use strict';
 let dayinput = document.getElementById('dayinput');
 let output = document.getElementById('output');
 const part = document.getElementById('radio_1');
 mainForm.addEventListener('submit', execute);
 
 function execute() {
+    let elem = document.getElementById("bar");
+
+    output.innerHTML = '';
     let endResult = 0;
-    dayinput.innerHTML = '';
     let inputSplitted = dayinput.value.trim().split(/\s+/);
     let threeElementCounter = 0;
     let value1 = 0;
     let value2 = 0;
     let value3 = 0;
-    inputSplitted.forEach(element => {
+
+    var i = 0, j = inputSplitted.length;
+    var iv = setInterval(function () {
+
         if (value1 === 0)
-            value1 = element;
+            value1 = inputSplitted[i];
         else if (value2 === 0)
-            value2 = element;
+            value2 = inputSplitted[i];
         else if (value3 === 0)
-            value3 = element;
+            value3 = inputSplitted[i];
 
         threeElementCounter++;
         if (threeElementCounter % 3 === 0) {
             if (isTriangle(value1, value2, value3)) {
-                output.innerHTML += `<p class="true">${value1} ${value2} ${value3}</p>`;
+                output.innerHTML = `<p class="true">${value1} ${value2} ${value3}</p>${output.innerHTML}`;
                 endResult++;
             }
             else
-                output.innerHTML += `<p class="false">${value1} ${value2} ${value3}</p>`;
+                output.innerHTML = `<p class="false">${value1} ${value2} ${value3}</p>${output.innerHTML}`;
             value1 = 0; value2 = 0; value3 = 0;
         }
-    });
-    output.innerHTML = `<h3>Result: ${endResult}</h3>`+ output.innerHTML;
-    // if (part.checked)
-    //     alert('1');
-    // else
-    //     alert('2');
+        //Set new progress bar percentage
+        elem.style.width = `${i / j * 100}%`;
+        
+        //Check if all elements have been processed
+        if (++i >= j) {
+            clearInterval(iv);
+            output.innerHTML = `<h3>Result: ${endResult}</h3>` + output.innerHTML;
+        }
+    }, 1);
 }
 
 function isTriangle(val1, val2, val3) {
